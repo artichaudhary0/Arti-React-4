9da035e50224227b4af9928ffedf142931b93a9e
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2025-01-24 16:56:18 +0530

    array function

---
+++ oprations.js
@@ -0,0 +1,61 @@
+/*
+Spreading
+merging
+destructuring
+map and key
+filter and reduce
+*/
+
+// Spreading ... allow elements or properties to be expanded into individual elements or properties.
+
+// let array = [1, 2, 2, 3, 4, 5];
+// let array3 = [1, 2, 2, 3, 4, 5];
+// let array2 = [...array, ...array3];
+
+// array2[3] = 234;
+
+// // console.log(array);
+// // console.log(array2);
+// // console.log(array3);
+
+// const obj1 = { a: 1, b: 2 };
+// const obj3 = { c: 1, d: 2 };
+// const obj2 = { ...obj1, ...obj3 };
+
+// obj2["a"] = 11;
+
+// console.log(obj1);
+// console.log(obj2);
+// console.log(obj3);
+
+// Merging : combining two or more array or object together.
+
+// destruct
+
+// let array1 = [3, 4, 5];
+
+// const [value1, value3] = array1;
+
+// console.log(value1);
+// value1 = 214;
+
+// console.log(value1);
+
+// const obj1 = { name: "XYZ", age: { a: 213 } };
+// const { age } = obj1;
+// console.log(age); // { a: 213 }
+// const { a } = age;
+// console.log(a);
+
+// map and key
+
+// const number = [2, 12, 312, 3, 132];
+// const cube = number.map((e) => e * e * e);
+// console.log(cube);
+
+// const item = ["fs", "sdf", "sf", "sdf"];
+// console.log(item[1]);
+
+
+
+
